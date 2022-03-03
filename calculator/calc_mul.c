8a2eec7dbcffe8928408d5f0ac1d9161b3f1331f
Uros Petkovic <uros.petkovic@syrmia.com>
2022-03-03 12:42:04 +0100

    Adding comment

--- calculator/calc_mul.c
+++ calculator/calc_mul.c
@@ -3,5 +3,8 @@
 #include "stdio.h"
 
 void calc_mul( int x, int y){
+        /* TODO: Update output text so it describes
+         * the operation in more details
+        */
 	printf( "Result = %d\r\n",x*y);
 }
